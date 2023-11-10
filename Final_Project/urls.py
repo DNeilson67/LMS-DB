080420424de21c3bb4830b25a57278cb3c973a73
Ëzo-Tanuki <michailbryanlie@gmail.com>
2023-11-10 21:09:31 +0700

    added authentication

--- Final_Project/urls.py
+++ Final_Project/urls.py
@@ -17,7 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+from authy.views import UserProfile
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/', include('authy.urls')),
     path('', include('students.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
